920b605ed045c78c816eb1eef45cb16b5fc10467
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-14 11:05:06 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "somlos", description: "DSL for application upgrades",
+                 version: :head, keywords: ["relex", "reltool", "systools","appup","relup","upgrade","downgrade"], 
+                 maintainers: [[name: "Yurii Rashkovskii", 
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/somlos"]])
+    
